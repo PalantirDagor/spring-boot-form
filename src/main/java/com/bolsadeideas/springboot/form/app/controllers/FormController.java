245a1b9efdee29ce06d4038318aa3c303f06f544
Duber Alexander Galvis Giraldo <duber.alexander@gmail.com>
2023-07-06 09:53:33 -0500

    Atributo object de Thymeleaf al form en la vista y automatizacion de mensajes error. video 58

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,8 +1,5 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -34,12 +31,12 @@ public class FormController {
 		
 		if(result.hasErrors()) {
 			
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err -> {
-				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			
-			model.addAttribute("error", errores);
+//			Map<String, String> errores = new HashMap<>();
+//			result.getFieldErrors().forEach(err -> {
+//				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+//			});
+//			
+//			model.addAttribute("error", errores);
 			return "form";
 		}
 		
